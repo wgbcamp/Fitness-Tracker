aa5e42e2e61635af2ebcc4f7a425c4b6d65bf999
Warren Brown <warrengbrn@gmail.com>
2020-03-26 03:37:15 -0400

    adding css framework

--- routes/api.js
+++ routes/api.js
@@ -18,6 +18,25 @@ router.get("/edit", function(req, res){
     res.render("edit");
 })
 
+router.get("/all", function(req, res){
+    db.plans.find({}, (error, data)=>{
+        if (error){
+            res.send(error)
+        }else{
+            var resultArray = [];
+            for(i=0; i<data.length; i++){
+                resultArray.push(data[i].name);
+                console.log("Results: " + data[i].name);
+            };
+            console.log(resultArray);
+            var hbsObject = {allresults: resultArray};
+            
+        }
+        res.render("viewall", hbsObject);
+    });
+    
+})
+
 router.get("/about", function(req, res){
     
     res.render("about");
@@ -34,5 +53,9 @@ router.post("/submit", (req, res)=>{
         }
     })
 })
+
+router.post("/all", (req, res)=>{
+    
+});
 module.exports = router;
 
